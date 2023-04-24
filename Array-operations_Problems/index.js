80ac1ea74b5010e8969a00481cc507bc2f2337ed
kotesh-arya <koteswarraomudila@gmail.com>
2023-04-24 13:51:57 +0530

    Q-2: Find the largest number in an array

--- Array-operations_Problems/index.js
+++ Array-operations_Problems/index.js
@@ -22,4 +22,20 @@ function insertIntoArray(arr, element, n, capacity, insertingPosition) {
     }
   }
 }
-console.log(insertIntoArray([1, 2, 3, 4], 9, 5, 7, 4));
+// console.log(insertIntoArray([1, 2, 3, 4], 9, 5, 7, 4));
+
+//  24 April 2023 Problems
+
+// [1] Find the largest element in the array
+
+const arr1 = [333,44545,3662,5];
+let highest = 0;
+for (let i=0;i<arr1.length;i++){
+  // console.log("previous index element:",arr1[i-1],"current index element:",arr1[i]);
+  console.log("CASE",i+1,"===>","highest",highest , "curr-val", arr1[i])
+  if(highest < arr1[i]){
+    highest = arr1[i];
+  }
+}
+console.log(highest, "highest number in the array")
+// console.log(Math.max(...arr1)); // Short-cut method
